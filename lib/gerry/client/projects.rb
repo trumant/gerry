5decb69ce130a4f17319cca57fa9f8f20b609879
Sebastian Schuberth <sschuberth@gmail.com>
2015-07-10 17:03:55 +0200

    Add methods to get / set the symbolic HEAD ref for a project

--- lib/gerry/client/projects.rb
+++ lib/gerry/client/projects.rb
@@ -16,6 +16,28 @@ module Gerry
       def find_project(name)
         get("/projects/#{name}")
       end
+
+      # Get the symbolic HEAD ref for the specified project.
+      #
+      # @param [String] project the project name.
+      # @return [String] the current ref to which HEAD points to.
+      def get_head(project)
+        get("/projects/#{project}/HEAD")
+      end
+
+      # Set the symbolic HEAD ref for the specified project to
+      # point to the specified branch.
+      #
+      # @param [String] project the project name.
+      # @param [String] branch the branch to point to.
+      # @return [String] the new ref to which HEAD points to.
+      def set_head(project, branch)
+        url = "/projects/#{project}/HEAD"
+        body = {
+          ref: 'refs/heads/' + branch
+        }
+        put(url, body)
+      end
     end
   end
 end
